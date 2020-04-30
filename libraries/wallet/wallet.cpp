50a8829034bde1505439ffa6400d40e85924d189
ioBanker <37595908+ioBanker@users.noreply.github.com>
2020-04-30 05:18:53 +0300

    Update wallet.cpp

--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2017 Cryptonomex, Inc., and contributors.
  *
- * The MIT License
+ * The MIT License 
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
