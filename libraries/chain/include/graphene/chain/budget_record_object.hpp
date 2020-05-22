609807f0ae6fb7ae94d53b2bb6791904f1867aa6
ioBanker <37595908+ioBanker@users.noreply.github.com>
2020-05-22 11:59:22 +0300

    Update budget_record_object.hpp

--- libraries/chain/include/graphene/chain/budget_record_object.hpp
+++ libraries/chain/include/graphene/chain/budget_record_object.hpp
@@ -52,7 +52,6 @@ struct budget_record
 
    // change in supply due to budget operations
    share_type supply_delta = 0;
-   share_type supply_delta_vote = 0;
 };
 
 class budget_record_object : public graphene::db::abstract_object<budget_record_object>
