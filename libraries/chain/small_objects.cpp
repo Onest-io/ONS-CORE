c12bffa3c98edf26529f082823ffc44916c0de4f
ioBanker <37595908+ioBanker@users.noreply.github.com>
2020-05-22 04:48:10 +0300

    Update small_objects.cpp

--- libraries/chain/small_objects.cpp
+++ libraries/chain/small_objects.cpp
@@ -53,6 +53,7 @@ FC_REFLECT_DERIVED_NO_TYPENAME(
    graphene::chain::budget_record, BOOST_PP_SEQ_NIL,
    (time_since_last_budget)
    (from_initial_reserve)
+   (from_initial_reserve_vote)
    (from_accumulated_fees)
    (from_unused_witness_budget)
    (requested_witness_budget)
